d7e8a97f4194423d910689b9ef649b80e53adbfe
吴宇春 <wuyuchun@rainbowcn.com>
2022-01-14 17:35:17 +0800

    feat(master): 修改tpg4j的版本为2021.4
    修改tpg4j的版本为2021.4

--- src/main/java/com/iwuyc/tools/demo/tpg4j/Bootstrap.java
+++ src/main/java/com/iwuyc/tools/demo/tpg4j/Bootstrap.java
@@ -2,9 +2,9 @@ package com.iwuyc.tools.demo.tpg4j;
 
 
 import com.google.common.util.concurrent.SettableFuture;
-import com.iwuyc.tools.tpg4j.thread.ThreadPoolService;
-import com.iwuyc.tools.tpg4j.thread.ThreadPoolServiceHolder;
-import com.iwuyc.tools.tpg4j.thread.conf.Config;
+import com.iwuyc.tpg4j.core.Tpg4jConfig;
+import com.iwuyc.tpg4j.core.Tpg4jFactory;
+import com.iwuyc.tpg4j.core.spi.Tpg4jService;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -13,7 +13,7 @@ public class Bootstrap {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // 配置文件。可以为空，如果为空，则使用默认的配置。
         String thframeYaml = null;
-        ThreadPoolService service = Config.config(thframeYaml);
+        Tpg4jService service = Tpg4jConfig.config(thframeYaml);
 
         // 方式1：由config返回的ThreadPoolsService实例获取线程池
         final SettableFuture<Void> end = SettableFuture.create();
@@ -25,12 +25,12 @@ public class Bootstrap {
         System.out.println(pools1);
 
         // 方式2：由ThreadPoolsServiceHolder获取线程池
-        ExecutorService pools2 = ThreadPoolServiceHolder.get(Bootstrap.class);
+        ExecutorService pools2 = Tpg4jFactory.get(Bootstrap.class);
         // print: true
         assert  pools1 == pools2;
         end.get();
 
         // 最后在应用程序关闭的时候，应当把线程池服务关闭。
-        ThreadPoolServiceHolder.getThreadPoolsService().shutdown();
+        Tpg4jFactory.getThreadPoolsService().shutdown();
     }
 }
